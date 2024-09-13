f8cca581300e5a26e188ddcd9076391a95f8ef93
Patrick Joseph Pronuevo <3xjoseph@gmail.com>
2024-09-13 17:10:37 +0800

    13. Debugging Tools
    - On Waypoint script, created a one line method that returns isPlaceable.
    
    - On CoordinateLabeler, created 2 methods ( ColorCoordinates and ToggleLabels)
    
    -- ColorCoordinates gives different colors on the label
    
    -- ToggleLabels, lets you activate the label (label is set by default)

--- Assets/Tiles/CoordinateLabeler.cs
+++ Assets/Tiles/CoordinateLabeler.cs
@@ -3,16 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using System;
+using UnityEngine.UI;
 
 [ExecuteAlways]
 public class CoordinateLabeler : MonoBehaviour
 {
+    [SerializeField] Color defaultColor = Color.white;
+    [SerializeField] Color blockColor = Color.gray;
+
     TextMeshPro label;
     Vector2Int coordinates = new Vector2Int();
+
+    Waypoint waypoint;
 
     void Awake() 
     {
         label = GetComponent<TextMeshPro>();
+        label.enabled = false;
+        waypoint = GetComponentInParent<Waypoint>();
     }
 
     void Update()
@@ -22,6 +30,28 @@ public class CoordinateLabeler : MonoBehaviour
             DisplayCoordinates();
             UpdateObjectName();
        } 
+
+       ColorCoordinates();
+       ToggleLabels();
+    }
+
+    void ToggleLabels()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            label.enabled = !label.IsActive();
+        }
+    }
+    void ColorCoordinates()
+    {
+        if (waypoint.IsPlaceable)
+        {
+            label.color = defaultColor;
+        }
+        else
+        {
+            label.color = blockColor;
+        }
     }
 
     void DisplayCoordinates()
